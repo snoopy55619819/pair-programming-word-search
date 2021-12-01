ba32fa249856a21bcf1b6781edf0c54b6c225509
snoopy55619819 <nikhiltallapureddy@gmail.com>
2021-12-01 23:21:00 +0000

    make updates

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,22 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'LARRY')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present vertically", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -33,6 +48,13 @@ describe("#wordSearch()", function() {
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
 
+
     assert.isTrue(result);
+  });
+  it("should return false if there is no spoon", function() {
+    const result = wordSearch([
+    ], 'SEINFELD')
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (!letters.length) return false;
+  
+    const horizontalJoin = joinToString(letters);
+    if (isWordInSplitArray(horizontalJoin, word))
+      return true;
+  
+    const verticalJoin = joinToString(transposeMatrix(letters));
+    console.log(verticalJoin);
+    if (isWordInSplitArray(verticalJoin, word))
+      return true;
+  
+    return false;
+  }
+  
+  const isWordInSplitArray = (arrayOfLetters, word) => {
+    for (letter of arrayOfLetters) {
+      if (letter.includes(word))
+        return true;
     }
-}
-
-module.exports = wordSearch
+  }
+  
+  const joinToString = (array) => array.map(ls => ls.join(''));
+  
+  const transposeMatrix = (matrix) => {
+    const resultMatrix = [];
+    for (const ind in matrix[0]) {
+      const newRow = [];
+      for (const arr of matrix) {
+        newRow.push(arr[ind]);
+      }
+      resultMatrix.push(newRow);
+    }
+    return resultMatrix;
+  }
+  module.exports = wordSearch
+  
+  
